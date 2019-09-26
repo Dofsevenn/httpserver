4b2fd2ee36d37c2754f364f7c209a93f3cb957dd
Kjetil <kjetil.dir@gmail.com>
2019-09-26 11:43:18 +0200

    Pom.xmn setup and dummytest

--- src/main/java/no/kristiania/HttpServer.java
+++ src/main/java/no/kristiania/HttpServer.java
@@ -2,4 +2,5 @@ package no.kristiania;
 
 public class HttpServer {
 
+
 }
--- src/test/java/no/kristiania/HttpServerTest.java
+++ src/test/java/no/kristiania/HttpServerTest.java
@@ -1,5 +1,13 @@
 package no.kristiania;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class HttpServerTest {
 
+    @Test
+    void mathshouldWork() {
+        assertEquals(5, 2+2);
+    }
 }
