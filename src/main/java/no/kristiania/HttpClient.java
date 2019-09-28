d517c9638343468bfed57342caec9f0f0919f1fa
Kjetil <kjetil.dir@gmail.com>
2019-09-28 16:38:04 +0200

    readLine(), outprint statusLine, HttpClientResponse constructor, getStatusCode
    *update getStatusCode to read the status code and return it
    *Made readLine() to read the inputStream

--- src/main/java/no/kristiania/HttpClient.java
+++ src/main/java/no/kristiania/HttpClient.java
@@ -26,11 +26,21 @@ public class HttpClient {
         socket.getOutputStream().flush();
 
         String statusLine = readLine(socket.getInputStream());
+        System.out.println(statusLine);
 
-        return new HttpClientResponse();
+        return new HttpClientResponse(statusLine);
     }
 
-    private String readLine(InputStream inputStream) {
-        return new StringBuilder().toString();
+    private String readLine(InputStream inputStream) throws IOException {
+        StringBuilder line = new StringBuilder();
+        int c;
+        while((c = inputStream.read()) != -1) {
+            if(c == '\r') {
+                inputStream.read();
+                break;
+            }
+            line.append((char)c);
+        }
+        return line.toString();
     }
 }
