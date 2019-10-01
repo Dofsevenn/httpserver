a3f1559b38ec9f7330c77dce1735ae3e346fbd2e
Kjetil <kjetil.dir@gmail.com>
2019-10-01 12:46:15 +0200

    Fix shouldReadHeather test, fix statusLine issue, refactor clientResponse constructor

--- src/main/java/no/kristiania/HttpClientResponse.java
+++ src/main/java/no/kristiania/HttpClientResponse.java
@@ -1,12 +1,31 @@
 package no.kristiania;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import static no.kristiania.HttpClient.readLine;
+
 public class HttpClientResponse {
 
     private String statusLine;
+    private Map<String, String> heathers = new HashMap<>();
 
-    public HttpClientResponse(String statusLine) {
+    public HttpClientResponse(InputStream inputStream) throws IOException {
 
-        this.statusLine = statusLine;
+        statusLine = readLine(inputStream);
+        System.out.println(statusLine);
+        String heatherLine;
+        while(!(heatherLine = readLine(inputStream)).isBlank()) {
+            int colonPos = heatherLine.indexOf(":");
+            String heatherName = heatherLine.substring(0, colonPos).trim();
+            String heatherValue = heatherLine.substring(colonPos+1).trim();
+            System.out.println("HeatherLine: " + heatherName + " ->" + heatherValue);
+            heathers.put(heatherName.toLowerCase(), heatherValue);
+        }
+        System.out.println();
+
     }
 
     public int getStatusCode() {
@@ -14,6 +33,6 @@ public class HttpClientResponse {
     }
 
     public String getHeather(String heatherName) {
-        return null;
+        return heathers.get(heatherName.toLowerCase());
     }
 }
--- src/test/java/no/kristiania/HttpClientTest.java
+++ src/test/java/no/kristiania/HttpClientTest.java
@@ -8,21 +8,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpClientTest {
 
+    private String URLECHO = "urlecho.appspot.com";
+
     @Test
     void shouldExecuteHttpRequest() throws IOException {
-        HttpClient client = new HttpClient("urlecho.appspot.com", 80, "/echo");
+        HttpClient client = new HttpClient(URLECHO, 80, "/echo");
         assertEquals(200, client.execute().getStatusCode());
     }
 
     @Test
     void shouldReadStatusCode() throws IOException {
-        HttpClient client = new HttpClient("urlecho.appspot.com", 80, "/echo?status=401");
+        HttpClient client = new HttpClient(URLECHO, 80, "/echo?status=401");
         assertEquals(401, client.execute().getStatusCode());
     }
 
     @Test
     void shouldReadHeathers() throws IOException {
-        HttpClient client = new HttpClient("urlecho.appspot.com", 80, "/echo?content-type=text/html");
-        assertEquals("test/html", client.execute().getHeather("content-type"));
+        HttpClient client = new HttpClient(URLECHO, 80, "/echo?content-type=text/plain");
+        assertEquals("text/plain; charset=utf-8", client.execute().getHeather("content-type"));
     }
 }
