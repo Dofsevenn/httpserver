7023336209a45554ff761e4d82c501202a61806c
Kjetil <kjetil.dir@gmail.com>
2019-10-01 23:10:52 +0200

    refactor HttpServerTest

--- src/test/java/no/kristiania/HttpServerTest.java
+++ src/test/java/no/kristiania/HttpServerTest.java
@@ -1,5 +1,6 @@
 package no.kristiania;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -8,18 +9,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpServerTest {
 
+    private HttpServer server;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        server = new HttpServer(0);
+        server.start();
+    }
+
     @Test
     void shouldGetStatusCode200() throws IOException {
-        HttpServer server = new HttpServer(0);
-        server.start();
         HttpClient client = new HttpClient("localhost", server.getPort(), "/echo");
         assertEquals(200, client.execute().getStatusCode());
     }
 
     @Test
     void shouldGetStatusCode401() throws IOException {
-        HttpServer server = new HttpServer(0);
-        server.start();
         HttpClient client = new HttpClient("localhost", server.getPort(), "/echo?status=401");
         assertEquals(401, client.execute().getStatusCode());
     }
